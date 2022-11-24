5a8f2659252b986c81996fa13739c02be8964c49
Pierre Joubert <pierrj@berkeley.edu>
2022-11-24 10:41:34 -0800

    shouldnt need these variables anymore

--- genome_annotation/launch_fungap.sh
+++ genome_annotation/launch_fungap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /global/scratch/users/pierrj/moryzae_virulence_project
+cd /global/scratch/users/pierrj/moryzae_virulence_project/genome_annotation
+
+cp -r /global/scratch/users/pierrj/fungap_runs/gladieux_all/template_run/ .
 
 while read genome; do
     echo $genome
@@ -9,10 +11,23 @@ while read genome; do
     fi
     mkdir ${genome}
     cd ${genome}
-        cp -r /global/scratch/users/pierrj/fungap_runs/gladieux_all/template_run/ .
+        cp -r ../template_run/fungap_out/ .
     cd ..
 done < strain_names
 
 while read genome; do
     sbatch --job-name=${genome}_run_fungap --export=genome=$genome /global/scratch/users/pierrj/moryzae_virulence/genome_annotation/run_fungap.slurm
+done < strain_names
+
+## to relaunch failed jobs due to busco download error
+
+squeue -u pierrj --format="%.100j" | tail -n +2 | awk '{print substr($1, 0,length($1)-11)}' > running_jobs
+
+while read genome; do
+    if grep -Fxq "$genome" running_jobs
+    then
+        echo "$genome is already running"
+    else
+    sbatch --job-name=${genome}_run_fungap --export=genome=$genome /global/home/users/pierrj/git/slurm/run_fungap.slurm
+    fi
 done < strain_names
